923606a33fdd85debf52e17dd01503721ae9699a
박장현 <p5725@cau.ac.kr>
2020-05-03 01:46:43 +0900

    변경

--- source.cpp
+++ source.cpp
@@ -3,118 +3,266 @@
 #include <time.h>
 #include <stdlib.h>
 
-SceneID scene0, scene1, scene2;
-ObjectID A, B, shop,item0_1 ;//scene1 요소들
-ObjectID initHeart[5];
-ObjectID gameHeart[5];
-ObjectID items[5]; //scene2 요소들
-TimerID timer1;
+#define FAIRY 0
+#define BOMB 1
+#define UP 2
+#define DOWN 3
+#define LEFT 4
+#define RIGHT 5
 
-int LHP[5] = { 100, 200, 300, 400, 500 }; // Left Heart Position
-int IP[5] = { 200, 300, 550, 800, 1000 }; // Item Position
+SceneID scene0, scene1, scene2,scene3,scene4;
+ObjectID title, start, help, play, goBack, A,B;
+ObjectID Items[6];
+ObjectID Heart[5]; 
+
+int HP[5] = { 100, 150, 200, 250, 300 }; // Left Heart Position
+int height = 600;
+int i = 4;
+int num;
+int score = 0;
+
+TimerID timer1, timer2, timer3;
+SoundID O, X;
 
 const Second animationTime = 0.01f;
 
-int i = 200;
+void GameOver() {
+	playSound(X);
+	hideObject(Heart[i]);
+	i--;
 
-void Launch(int j) {
-	enterScene(scene1);
-
-	switch(j) {
-	case 0: // 단검을 눌렀을때
-		showObject(item0_1);
-		locateObject(item0_1, scene1, i, 150);
-		setTimer(timer1, animationTime);
-		startTimer(timer1);
-
+	if (i == -1) {
+		enterScene(scene2);
+		stopTimer(timer2);
 	}
-		
 }
 
-void HookPick(int j) {
-	switch (j) {
-	case 0: Launch(0); break;
-	//case 1: Launch 
+int Random() {
+	srand((unsigned int)time(NULL));
+	num = rand() % 6;
+	return num;
+}
+
+void Fall(int j) {
+	if (j == FAIRY) {
+		if (height > 0) { 
+			height = height - 10;
+			locateObject(Items[FAIRY], scene1, 600, height);
+			setTimer(timer1, animationTime);
+			startTimer(timer1);
+			
+			if (height <= 0) {
+				hideObject(Items[FAIRY]);
+				height = 600;
+				Fall(Random());
+				showObject(Items[Random()]);
+				
+			}
+
+		}
+	}
+
+	else if (j== BOMB){
+		if (height > 0) {
+			height = height - 30;
+			locateObject(Items[BOMB], scene1, 600, height);
+			setTimer(timer1, animationTime);
+			startTimer(timer1);
+			
+		}
+			if (height <= 0) {
+				hideObject(Items[BOMB]);
+				height = 600;
+				Fall(Random());
+				showObject(Items[Random()]);
+				
+			}
+	}
+
+	else if (j == UP) {
+		if (height > 0) {
+			height = height - 40;
+			locateObject(Items[UP], scene1, 600, height);
+			setTimer(timer1, animationTime);
+			startTimer(timer1);
+
+		}
+		if (height <= 0) {
+			hideObject(Items[UP]);
+			height = 600;
+			Fall(Random());
+			showObject(Items[Random()]);
+
+		}
+	}
+
+	else if (j == DOWN) {
+		if (height > 0) {
+			height = height - 20;
+			locateObject(Items[DOWN], scene1, 600, height);
+			setTimer(timer1, animationTime);
+			startTimer(timer1);
+
+		}
+		if (height <= 0) {
+			hideObject(Items[DOWN]);
+			height = 600;
+			Fall(Random());
+			showObject(Items[Random()]);
+		}
 
 	}
 
+	else if (j == LEFT) {
+		if (height > 0) {
+			height = height - 50;
+			locateObject(Items[LEFT], scene1, 600, height);
+			setTimer(timer1, animationTime);
+			startTimer(timer1);
+
+			if (height <= 0) {
+				hideObject(Items[LEFT]);
+				height = 600;
+				Fall(Random());
+				showObject(Items[Random()]);
+
+			}
+
+		}
+	}
+
+	else if (j == RIGHT) {
+		if (height > 0) {
+			height = height - 30;
+			locateObject(Items[RIGHT], scene1, 600, height);
+			setTimer(timer1, animationTime);
+			startTimer(timer1);
+
+			if (height <= 0) {
+				hideObject(Items[RIGHT]);
+				height = 600;
+				Fall(Random());
+				showObject(Items[Random()]);
+
+			}
+
+		}
+	}
 
 
+}
+
+void timerCallback(TimerID timer) {
+		Fall(num);
+
+		if (timer == timer2) {
+			enterScene(scene4);
+			increaseTimer(timer3, score);
+			showTimer(timer3);
+		}
+}
+
+void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
+	if (object == start) {
+		enterScene(scene1);
+		showTimer(timer2);
+	}
+
+	if (object == play) {
+		startTimer(timer2);
+		Fall(Random());
+		showObject(Items[Random()]);
+		hideObject(play);
+	}
+
+	if (object == help) {
+		enterScene(scene3);
+	}
+
+	if (object == goBack) {
+		enterScene(scene0);
+	}
+
+	if (object == Items[FAIRY] || object == Items[LEFT]) {
+		if (action == MouseAction::MOUSE_DRAG_LEFT) {
+			playSound(O);
+			hideObject(Items[FAIRY]);
+			hideObject(Items[LEFT]);
+			score++;
+		}
+
+		else {
+			GameOver();
+		}
+	}
+
+	if (object == Items[BOMB]|| object == Items[RIGHT]) {
+		if (action == MouseAction::MOUSE_DRAG_RIGHT) {
+			playSound(O);
+			hideObject(Items[BOMB]);
+			hideObject(Items[RIGHT]);
+			score++;
+		}
+
+		else {
+			GameOver();
+		}
+	}
+
+	if (object == Items[DOWN]) {
+		if (action == MouseAction::MOUSE_DRAG_UP) {
+			playSound(O);
+			hideObject(Items[DOWN]);
+			score++;
+		}
+
+		else {
+			GameOver();
+		}
+	}
+
+	if (object == Items[UP]) {
+		if (action == MouseAction::MOUSE_DRAG_DOWN) {
+			hideObject(Items[UP]);
+			playSound(O);
+			score++;
+		}
+
+		else {
+			GameOver();
+		}
+	}
+	
 }
 
 void game_init() {
 
-	char heartImage_left[20];
-	
-	for (int k = 0; k < 5; k++) {
-		sprintf_s(heartImage_left, "RH%d.png", k);
-		initHeart[k] = createObject(heartImage_left);
-		gameHeart[k] = initHeart[k];
-		locateObject(gameHeart[k], scene1, LHP[k],550);
-		scaleObject(gameHeart[k], 0.7);
-		showObject(gameHeart[k]);
-	}
-	char itemImage[20];
-
-	for (int f = 0; f < 5; f++) {
-		sprintf_s(itemImage, "item%d.png", f);
-		items[f] = createObject(itemImage);
-		locateObject(items[f], scene2, IP[f], 100);
-		showObject(items[f]);
-		scaleObject(items[f], 0.7);
-	}
-
-
-}
-	
-
-
-
-
-void timerCallback(TimerID timer) {
-
-	if (i > 0 && i < 900) {
-		i = i + 40;
-		setTimer(timer1, animationTime);
-		startTimer(timer1);
-		Launch(0);
-		if (i >= 900) {
-			hideObject(item0_1);
-			i = 0;
-		}
-
-		
-
-
-			
-
-	}
-
-	
-}
-
-void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
-	if(object == items[0]){
-		launch(0);
-		i = 200;
-		}
-
-	if (object == shop) {
-		enterScene(scene2);
-	}
-	
-	
-}
-
-
-int main() {
-
-	setMouseCallback(mouseCallback);
-
 	scene0 = createScene("후크 선장과의 결투", "후크vs피터팬.jpg");
-	scene1 = createScene("후크 선장과의 결투", "전투창.jpg");
-	scene2 = createScene("아이템 선택", "상점창.jpg");
+	
+	title = createObject("제목.png");
+	locateObject(title, scene0, 350, 500);
+	showObject(title);
+	scaleObject(title, 3);
 
+
+	start = createObject("시작.png");
+	locateObject(start, scene0, 400, 100);
+	showObject(start);
+	scaleObject(start, 1.5);
+
+	help = createObject("방법.png");
+	locateObject(help, scene0, 700, 100);
+	showObject(help);
+	scaleObject(help, 1.5);
+
+
+	scene1 = createScene("후크 선장과의 결투", "전투창.jpg");
+
+	play = createObject("플레이.png");
+	locateObject(play, scene1, 600, 100);
+	showObject(play);
+	scaleObject(help, 1.5);
+	
 
 	A = createObject("피터팬.png");
 	locateObject(A, scene1, 100, 0);
@@ -124,24 +272,48 @@ int main() {
 	locateObject(B, scene1, 900, 0);
 	showObject(B);
 
-	shop = createObject("상점.png");
-	locateObject(shop, scene1, 600, 550);
-	showObject(shop);
-	scaleObject(shop, 0.5f);
+	char heartImage[20];
+	for (int k = 0; k < 5; k++) {
+		sprintf_s(heartImage, "RH%d.png", k);
+		Heart[k] = createObject(heartImage);
+		locateObject(Heart[k], scene1, HP[k],600 );
+		scaleObject(Heart[k], 0.5);
+		showObject(Heart[k]);
+	}
 
+	char itemImage[20];
+	for (int k = 0; k < 6; k++) {
+		sprintf_s(itemImage, "item%d.png", k);
+		Items[k] = createObject(itemImage);
+		scaleObject(Items[k], 0.6);
+		showObject(Items[k]);
+	}
 
-	item0_1 = createObject("단검_왼-오.png");
-	locateObject(item0_1, scene2, 100, 40);
-	hideObject(item0_1);
-	scaleObject(item0_1, 0.5f);
+	O = createSound("O.wav");
+	X = createSound("X.wav");
 
+	scene2 = createScene("게임오버", "게임오버.jpg");
+
+	scene3 = createScene("게임방법", "방법창.jpg");
+
+	goBack = createObject("돌아가기.png");
+	locateObject(goBack, scene3, 1000, 600);
+	showObject(goBack);
+	scaleObject(goBack, 1.5);
+
+	scene4 = createScene("당신의 점수", "스코어창.jpg");
+	
+}
+
+int main() {
 
 	game_init();
-
-
+	setMouseCallback(mouseCallback);
 	setTimerCallback(timerCallback);
-	timer1 = createTimer(10.0f);
-	startTimer(timer1);
-	startGame(scene1);
+	timer1 = createTimer(60.0f);
+	timer2 = createTimer(60.0f);
+	timer3 = createTimer(0.0f);
 
+	startGame(scene0);
+	
 }
